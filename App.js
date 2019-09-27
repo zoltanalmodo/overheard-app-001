f04e011653778c62eaeaf4cbb88ae7e52a28499a
zoltan_almodo <zoltanalmodo@gmail.com>
2019-09-27 22:34:15 +0100

    using regular system font

--- App.js
+++ App.js
@@ -1,29 +1,8 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-import * as Font from 'expo-font';
-
 import DealsScreen from './src/screens/DealsScreen/DealsScreen';
 import LoadingScreen from './src/screens/LoadingScreen/LoadingScreen';
-
-
-export default class App extends React.Component {
-
-  state = {
-    fontLoaded: false,
-  };
-
-  async componentDidMount() {
-    await Font.loadAsync({
-      'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf'),
-      'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
-    });
-
-    this.setState({ fontLoaded: true });
-  }
-
-  // ...
-}
 
 
 const navigator = createStackNavigator (
