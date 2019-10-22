3c3d35f813d03e8c10be5e8a5a7a02c04d9b1096
zoltan_almodo <zoltanalmodo@gmail.com>
2019-10-22 18:14:02 +0100

    new login server (NO ngrok) = works ! hurray !

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,10 +2,6 @@ import axios from 'axios';
 
 export default axios.create ({
 
-    // =============================================== WITH ngrok
-    // baseURL: 'http://68eb2be5.ngrok.io/api/auth'
-    // =============================================== WITH ngrok
-
     // =============================================== NOOOO ngrok
     baseURL:'http://206.189.28.178:5000/api/auth'
     // =============================================== NOOOO ngrok
